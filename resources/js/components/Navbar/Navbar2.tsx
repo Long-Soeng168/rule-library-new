7e779a5f740d417c8aa392918777e6a2ee35f337
Long-Soeng167 <generalpurpose167@gmail.com>
2026-01-13 08:35:55 +0700

    update navbar

--- resources/js/components/Navbar/Navbar2.tsx
+++ resources/js/components/Navbar/Navbar2.tsx
@@ -9,22 +9,22 @@ const Navbar2 = () => {
     return (
         <>
             {/* Start Top Navbar */}
-            <div className="min-[650px]:border-b">
+            <div className="min-[1000px]:border-b">
                 <div className="section-container mx-auto w-full py-4">
                     <div className="flex h-full items-center justify-between">
                         <NavbarLogo2 />
 
-                        <div className="max-[650px]:hidden">
+                        <div className="max-[1000px]:hidden">
                             <NavMenu2 />
                         </div>
-                        <div className="max-[650px]:hidden">
+                        <div className="max-[1000px]:hidden">
                             <div className="flex items-center gap-3">
                                 <LibrarySearchSheet />
                                 <SwitchDarkModeSmoothAnimated />
                                 <NavLanguage />
 
                                 {/* Start Mobile Menu */}
-                                <div className="hidden max-[650px]:block">
+                                <div className="hidden max-[1000px]:block">
                                     <NavSheet />
                                 </div>
                                 {/* End Mobile Menu */}
@@ -36,7 +36,7 @@ const Navbar2 = () => {
             {/* End Top Navbar */}
 
             {/* Start Bottom Navbar */}
-            <div className="z-30 mx-auto w-full border-b bg-background pb-4 min-[650px]:hidden">
+            <div className="z-30 mx-auto w-full border-b bg-background pb-4 min-[1000px]:hidden">
                 <nav className="section-container">
                     <div className="flex h-full items-center justify-between">
                         <div></div>
@@ -47,7 +47,7 @@ const Navbar2 = () => {
                             <NavLanguage />
 
                             {/* Start Mobile Menu */}
-                            <div className="hidden max-[650px]:block">
+                            <div className="hidden max-[1000px]:block">
                                 <NavSheet />
                             </div>
                             {/* End Mobile Menu */}
