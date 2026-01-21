b31490c36133d5325f5935002d0cb29897d18f13
Long-Soeng167 <generalpurpose167@gmail.com>
2026-01-21 12:59:29 +0700

    update staff

--- resources/js/components/Card/StaffCard.tsx
+++ resources/js/components/Card/StaffCard.tsx
@@ -17,7 +17,7 @@ export default function StaffCard({ name, name_kh, role, imageUrl }) {
             {/* Name & Role */}
             <div className="p-2 text-center">
                 <h3 className="text-[clamp(0.75rem,2.5vw,1rem)] font-semibold whitespace-nowrap text-foreground">{name_kh}</h3>
-                <h3 className="text-[clamp(0.65rem,2vw,0.875rem)] whitespace-nowrap text-muted-foreground">{name}</h3>
+                <h3 className="text-[clamp(0.65rem,2vw,0.875rem)] whitespace-nowrap text-muted-foreground">{role}</h3>
                 {/* <p className="mt-1 text-xs text-gray-500">{role}</p> */}
             </div>
         </div>
