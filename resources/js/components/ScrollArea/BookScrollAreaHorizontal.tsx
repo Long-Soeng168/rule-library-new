cdcfa7e7c636bd61d1b0fa2d78b98bdc5c42a0bd
Long-Soeng167 <generalpurpose167@gmail.com>
2026-03-03 11:23:03 +0700

    update image cover fallback

--- resources/js/components/ScrollArea/BookScrollAreaHorizontal.tsx
+++ resources/js/components/ScrollArea/BookScrollAreaHorizontal.tsx
@@ -1,10 +1,15 @@
 import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
+import useTranslation from '@/hooks/use-translation';
+import { cn } from '@/lib/utils';
 import { Link } from '@inertiajs/react';
+import { ImageIcon } from 'lucide-react';
 import HoverButton from '../Button/HoverButton';
 import HoverButton2 from '../Button/HoverButton2';
 import BooksCarousel from '../Carousel/BooksCarousel';
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
 
 export function BookScrollAreaHorizontal({ items, mainCategoryCode }: { items: any[]; mainCategoryCode: string }) {
+    const { currentLocale } = useTranslation();
     return (
         <>
             {/* Mobile */}
@@ -16,11 +21,28 @@ export function BookScrollAreaHorizontal({ items, mainCategoryCode }: { items: a
                                 <Link key={item.id} href={`/resources/${item.main_category_code}/${item.id}`}>
                                     <div className="w-[265px] shrink-0">
                                         <div className="overflow-hidden rounded-md border duration-300 hover:border-primary">
-                                            <img
-                                                src={`/assets/images/items/thumb/${item.thumbnail}`}
-                                                alt={item.name}
-                                                className="h-[378.57px] w-[265px] object-cover"
-                                            />
+                                            <Avatar className="h-full w-full rounded-none border-none">
+                                                <AvatarImage
+                                                    src={`/assets/images/items/thumb/${item.thumbnail}`}
+                                                    className="h-[378.57px] w-[265px] object-cover"
+                                                    alt={item.name}
+                                                />
+                                                <AvatarFallback className="relative flex h-[378.57px] w-[265px] flex-col items-start justify-between overflow-hidden rounded-none bg-zinc-50 p-4 text-muted-foreground dark:bg-zinc-950">
+                                                    <div
+                                                        className={cn(
+                                                            'transition-color flex h-full w-full flex-col items-center justify-center rounded-xl p-8',
+                                                        )}
+                                                    >
+                                                        <ImageIcon className="size-12" strokeWidth={1.25} />
+
+                                                        <div className="mt-4 flex flex-col items-center gap-1 text-center">
+                                                            <span className="text-base text-foreground">
+                                                                {currentLocale == 'kh' ? 'មិនមានរូបភាព' : 'No Image Available'}
+                                                            </span>
+                                                        </div>
+                                                    </div>
+                                                </AvatarFallback>
+                                            </Avatar>
                                         </div>
 
                                         <p className="line-clamp-3 w-full pt-2 text-base whitespace-pre-wrap text-foreground">
